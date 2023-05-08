173d01b8ceb21ba69c8f4e19002af5f3a87f8c30
Ștefan-Cosmin Ciocan <57830279+cosminc98@users.noreply.github.com>
2023-05-08 18:44:36 +0000

    Install librosa ffmpeg and libsnfile on devcontainer

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,2 +1,3 @@
-python3.9 -m pip install flask Flask-cors
+python3.9 -m pip install flask Flask-cors librosa
+sudo apt -y install libsndfile-dev ffmpeg
 echo "VITE_INFERENCE_API_URL=\"https://${CODESPACE_NAME}-4242.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}\"" > website/.env
