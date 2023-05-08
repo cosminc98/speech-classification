a973dcad45d1813d24ae8a92016641dfa9c1ebf4
Ștefan-Cosmin Ciocan <57830279+cosminc98@users.noreply.github.com>
2023-05-08 18:20:34 +0000

    Convert opus codec in webm container to pcm16 16kHz wave

--- inference/server.py
+++ inference/server.py
@@ -2,16 +2,26 @@ import os
 from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
 import requests
+import tempfile
+import librosa
+import soundfile
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/predict', methods=['POST'])
 def predict_on_audio():
-    request.files['audio'].save("tmp")
-    # print("Posted file: {}".format(request.files['file']))
-    # file = request.files['file']
-    # return ""
+    tmp_dirname = tempfile.TemporaryDirectory()
+
+    audio_fpath = os.path.join(tmp_dirname.name, "sample.webm")
+    request.files['audio'].save(audio_fpath)
+
+    audio, sr = librosa.load(audio_fpath, sr=16000)
+    wav_fpath = os.path.join(tmp_dirname.name, "sample.wav")
+    soundfile.write(file=wav_fpath, data=audio, samplerate=sr, subtype="PCM_16")
+
+    tmp_dirname.cleanup()
+
     response = jsonify({'some': 'data'})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
